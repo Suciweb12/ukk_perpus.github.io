9776e43a8c9fbf330248975733d71f805a0713ab
Suciweb12 <suci20020@gmail.com>
2024-02-29 14:47:34 +0700

    first commit

---
+++ uci.php
@@ -0,0 +1,25 @@
+<body>
+    <style>
+     body {
+            background-image: url('assets/img/perpus.jpg'); /* Ganti dengan path gambar Anda */
+           
+    
+            color: #fff; /* Warna teks */
+            padding-right: 40px;
+            background-position: center;
+            padding-top: 0px; /* Ruang di bagian atas untuk memperhitungkan navbar */
+            background-repeat: no-repeat;
+	        margin: auto;
+	        background-attachment: fixed;
+            background-size: 100% 100% ;
+        }    
+        .card {
+            background-color: whitesmoke; /* Warna latar belakang semi-transparan untuk konten */
+            color: white; /* Warna teks untuk konten */
+        }
+        /* Tambahkan warna pada tabel */
+       
+    </style>
+</head>
+</body>
+</html>
